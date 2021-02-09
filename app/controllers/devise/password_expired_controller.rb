6be50d36680dff92b3e2accad2084c800569f85a
Vitalii <kaktusyaka@users.noreply.github.com>
2021-02-09 13:27:14 +1000

    added skip redirect to all controllers in this gem

--- app/controllers/devise/password_expired_controller.rb
+++ app/controllers/devise/password_expired_controller.rb
@@ -1,6 +1,7 @@
 class Devise::PasswordExpiredController < DeviseController
   skip_before_action :handle_password_change
   before_action :skip_password_change, only: [:show, :update]
+  skip_before_action :redirect_to_ca_learner 
   prepend_before_action :authenticate_scope!, :only => [:show, :update]
 
   def show
